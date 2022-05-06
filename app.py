65b30e9637eb9c4e6ee0f2478977081b46c98255
maseratimatti <80660424+maseratimatti@users.noreply.github.com>
2022-05-06 15:26:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,32 @@ import dash
 from dash import dcc
 from dash import html 
 from dash.dependencies import Input, Output, State
+
+import dash_auth
+
 import plotly.express as px
 
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/maseratimatti/Curvex-dash-grp13/main/Curvexdashdata.csv") #måske brug sep=';'
+df = pd.read_csv("https://raw.githubusercontent.com/maseratimatti/Curvex-dash-grp13/main/Curvexdashdata.csv", sep=';') #måske brug sep=';'
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+auth = dash_auth.BasicAuth(
+    app,
+    {'113': 'KEA'}
+)
 
 app.layout = html.Div([
     html.H1("Målings Rapport for bruger 113", style={"textAlign":"center"}),
     html.Hr(),
     html.P("Vælg hjernefrekvens data:"),
     html.Div(html.Div([
-        dcc.Dropdown(id='type', clearable=False,
+        dcc.Dropdown(id='Frekvens', clearable=False,
                      value="Gamma",
                      options=[{'label': x, 'value': x} for x in
-                              df["type"].unique()]),
+                              df["Frekvens"].unique()]),
     ],className="two columns"),className="row"),
 
     html.Div(id="output-div", children=[]),
@@ -27,17 +35,26 @@ app.layout = html.Div([
 
 
 @app.callback(Output(component_id="output-div", component_property="children"),
-              Input(component_id="type", component_property="value"),
+              Input(component_id="Frekvens", component_property="value"),
 )
-def make_graphs(type_chosen):
+def make_graphs(Frekvens_chosen):
     # HISTOGRAM
-    df_hist = df[df["type"]==type_chosen]
+    df_hist = df[df["Frekvens"]==Frekvens_chosen]
     fig_hist = px.histogram(df_hist, x="Værdi")
     fig_hist.update_xaxes(categoryorder="total descending")
+
+    df_line = df[df["Frekvens"]==Frekvens_chosen]
+    fig_line = px.line(df_line,
+        x="Tidspunkt", y="Måling", color='Frekvens', markers=True)
 
     return [
         html.Div([
             html.Div([dcc.Graph(figure=fig_hist)], className="six columns"),
+        ], className="row"),
+        html.H2("Tidspunkter", style={"textAlign":"center"}),
+        html.Hr(),
+        html.Div([
+            html.Div([dcc.Graph(figure=fig_line)], className="twelve columns"),
         ], className="row"),
     ]
 
